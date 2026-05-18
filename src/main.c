ed3eedbae7996ecc7a0dc450d73624a459463b05
TheGreatYagi <>
2026-05-17 22:21:13 -0500

    added level system

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 
 const int w = 1280;
 const int h = 800;
+const int WIN_SCORE = 3;
 
 // set colors
 // https://rgbcolorpicker.com/
@@ -29,18 +30,20 @@ int main() {
 	Score score = {0,0};
 	//printf("Ball directions, x: %d, y:%d\n",ball.vel_x, ball.vel_y);
 	bool gameStart = 0;
+	int level = 1;
 	while (!WindowShouldClose()) { 
 		//setup game
 		while(!gameStart) { //title screen
 			BeginDrawing();
 			ClearBackground(bgColor);
-			DrawText(TextFormat("Press Space to start!\nFirst to 5 wins!"), GetScreenWidth()/2, GetScreenHeight()/2, 35, RAYWHITE);
+			DrawText(TextFormat("Press Space to start!\nFirst to %d wins!", WIN_SCORE), GetScreenWidth()/2, GetScreenHeight()/2, 35, RAYWHITE);
 			if (GetKeyPressed() == KEY_SPACE) {
 				gameStart =1;
 			}
 			EndDrawing();
 		}
 		//Start level
+
 		BeginDrawing();
 		ClearBackground(bgColor);
 
@@ -55,7 +58,60 @@ int main() {
 		} 
 
 		// Update comm movement to track ball
-		updateComm(&comm, &ball);
+		if (level == 1) {
+			if (score.player == WIN_SCORE)  {
+				// increment game level and display congrats
+				level += 1;
+				while(GetKeyPressed() != KEY_SPACE) {
+					ClearBackground(bgColor);
+					DrawText(TextFormat("Congrats, you've won!\nPress Space for Level 2!"), GetScreenWidth()/2, GetScreenHeight()/2, 35, RAYWHITE);
+					EndDrawing();
+				}
+				score.player = 0;
+				score.comm = 0;
+				continue;
+			}
+			if (score.comm == WIN_SCORE) {
+				while(GetKeyPressed() != KEY_SPACE) {
+					ClearBackground(bgColor);
+					DrawText(TextFormat("Sorry, you lose!\nPress space to exit"), GetScreenWidth()/2, GetScreenHeight()/2, 35, RAYWHITE);
+					EndDrawing();
+				}
+				CloseWindow();
+				return 0;
+			}
+			if ((GetRandomValue(0,100000)) > 82000) {
+				updateComm(&comm, &ball);
+				}
+		}
+		if (level == 2) {
+			if (score.player == WIN_SCORE)  {
+				// increment game level and display congrats
+				level += 1;
+				while(GetKeyPressed() != KEY_SPACE) {
+					ClearBackground(bgColor);
+					DrawText(TextFormat("Congrats, you've beat the game!\nPress Space to exit!"), GetScreenWidth()/2, GetScreenHeight()/2, 35, RAYWHITE);
+					EndDrawing();
+				}
+					CloseWindow();
+					return 0;
+			}
+			if (score.comm == WIN_SCORE) {
+				while(GetKeyPressed() != KEY_SPACE) {
+					ClearBackground(bgColor);
+					DrawText(TextFormat("Sorry, you lose!\nPress space to exit"), GetScreenWidth()/2, GetScreenHeight()/2, 35, RAYWHITE);
+					EndDrawing();
+				}
+				CloseWindow();
+				return 0;
+			}
+			if ((GetRandomValue(0,100000)) > 80000) {
+				updateComm(&comm, &ball);
+				}
+		}
+		
+		
+		//updateComm(&comm, &ball);
 		
 		//update ball
 		updateBall(&ball, &player, &comm, &score);
@@ -71,6 +127,7 @@ int main() {
 		//loop
 		EndDrawing();
 	}
+	
 
 	return 0;
 }
